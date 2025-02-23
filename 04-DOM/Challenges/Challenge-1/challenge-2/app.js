713c31f4240aaa6bc1d4e1f114d6789c9fc45b15
priyansudash1999 <1999priyansudash@gmail.com>
2025-02-23 14:02:02 +0530

    complete challenge-2 chnage the heading according to btn bg color

--- 04-DOM/Challenges/Challenge-1/challenge-2/app.js
+++ 04-DOM/Challenges/Challenge-1/challenge-2/app.js
@@ -1,3 +1,28 @@
 /**
- * Write your challenge solution here
+ * JavaScript for changing text color using switch case
  */
+
+const mainHeading = document.getElementById('mainHeading');
+
+document.querySelector('.color-buttons').addEventListener('click',  (event) => {
+  let color = event.target.id; 
+  switch (color) {
+    case 'redButton':
+      mainHeading.style.color = '#e74c3c';
+      break;
+    case 'greenButton':
+      mainHeading.style.color = '#2ecc71';
+      break;
+    case 'blueButton':
+      mainHeading.style.color = '#3498db';
+      break;
+    case 'purpleButton':
+      mainHeading.style.color = '#9b59b6';
+      break;
+    case 'resetButton':
+      mainHeading.style.color = '#000'; 
+      break;
+    default:
+      console.log('banado aapke marzi se button');
+    }
+});
