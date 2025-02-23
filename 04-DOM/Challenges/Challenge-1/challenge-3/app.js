ab71a433bcea198fcc7948a9f5bc48245a8cd5a7
priyansudash1999 <1999priyansudash@gmail.com>
2025-02-23 18:17:12 +0530

    complete assignment-3

--- 04-DOM/Challenges/Challenge-1/challenge-3/app.js
+++ 04-DOM/Challenges/Challenge-1/challenge-3/app.js
@@ -1,3 +1,25 @@
 /**
  * Write your challenge solution here
  */
+const name = document.getElementById('nameInput')
+const job = document.getElementById('jobInput')
+const age = document.getElementById('ageInput')
+const bio = document.getElementById('bioInput')
+
+let nameDisplay = document.getElementById('nameDisplay')
+let jobDisplay = document.getElementById('jobDisplay')
+let ageDisplay = document.getElementById('ageDisplay')
+let bioDisplay = document.getElementById('bioDisplay')
+
+name.addEventListener('input', (e) => {
+  nameDisplay.innerText = e.target.value
+})
+job.addEventListener('input', (e) => {
+  jobDisplay.innerText = e.target.value
+})
+age.addEventListener('input', (e) => {
+  ageDisplay.innerText = e.target.value
+})
+bio.addEventListener('input', (e) => {
+  bioDisplay.innerText = e.target.value
+})
