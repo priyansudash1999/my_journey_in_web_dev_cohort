0dceaee6d348d7dfebb74d9ed3d2f64d33bc7479
priyansudash1999 <1999priyansudash@gmail.com>
2025-02-26 10:18:11 +0530

    complete wallpaper caraousel

--- 04-DOM/Challenges/Challenge-1/challenge-5/app.js
+++ 04-DOM/Challenges/Challenge-1/challenge-5/app.js
@@ -20,3 +20,83 @@ const images = [
     caption: 'Urban City Skyline',
   },
 ];
+
+const image_div = document.getElementById('carouselTrack')
+const caption = document.getElementById('caption')
+const prev_btn = document.getElementById("prevButton")
+const next_btn = document.getElementById('nextButton')
+const carouselNav = document.getElementById('carouselNav')
+const auto_play_btn = document.getElementById('autoPlayButton')
+const timer = document.getElementById('timerDisplay')
+
+let currInd = 0
+let intervalId = null
+
+function displayImage() {
+  image_div.innerHTML = ''
+  let image = document.createElement('img')
+  image.src = images[currInd].url
+  image.style.width = '100%'
+  image_div.appendChild(image)
+
+  caption.innerText = images[currInd].caption
+  updateIndicators()
+}
+
+function updateIndicators() {
+  carouselNav.innerHTML = ""; 
+  images.forEach((_, ind)=>{
+    const circle = document.createElement('span')
+    circle.classList.add('carousel-indicator')
+    if(ind === currInd){
+      circle.classList.add('active')
+    }
+    circle.addEventListener('click', () => {
+      currInd = ind
+      displayImage()
+    })
+    carouselNav.appendChild(circle)
+  })
+}
+function nextImage(){
+  currInd = (currInd + 1) % images.length
+  displayImage()
+}
+
+function prevImage(){
+  currInd = (currInd - 1 + images.length) % images.length
+  displayImage()
+}
+
+function autoPlayImages(){
+  if(intervalId) return
+  intervalId = setInterval(() => {
+    currInd = (currInd + 1) % images.length; 
+    displayImage();
+  }, 5000)
+}
+
+function stopAutoPlay(){
+  clearInterval(intervalId)
+  intervalId = null
+}
+
+next_btn.addEventListener('click', () => {
+  nextImage()
+})
+prev_btn.addEventListener('click', () => {
+  prevImage()
+})
+auto_play_btn.addEventListener('click', () => {
+  if(auto_play_btn.innerText === 'Start Auto Play'){
+    auto_play_btn.innerText = 'Stop Auto Play'
+    autoPlayImages()
+  }
+  else{
+     auto_play_btn.innerText = 'Start Auto Play'
+     stopAutoPlay()
+     timer.innerText = ''
+  }
+})
+
+displayImage();
